ba0ef870ad5fe2fbae65d9835c6f30367ea370e6
teagang <42443593+teagang@users.noreply.github.com>
2020-12-27 21:36:40 +0200

    added sitename to captcha

--- index.js
+++ index.js
@@ -26,14 +26,8 @@ function onSubmit(token) {
 {
     "success": true|false,
     "challenge_ts": timestamp,  // timestamp of the challenge load (ISO format yyyy-MM-dd'T'HH:mm:ssZZ)
-    "hostname": string,         // the hostname of the site where the reCAPTCHA was solved
+    "hostname": steppingstonepsychology.com,         // the hostname of the site where the reCAPTCHA was solved
     "error-codes": [...]        // optional
   }
 
-  /*for android*/
-  {
-    "success": true|false,
-    "challenge_ts": timestamp,  // timestamp of the challenge load (ISO format yyyy-MM-dd'T'HH:mm:ssZZ)
-    "apk_package_name": string, // the package name of the app where the reCAPTCHA was solved
-    "error-codes": [...]        // optional
-  }
+  
