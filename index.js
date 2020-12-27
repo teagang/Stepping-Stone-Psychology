3f93b7e9f2a07563f35058cfa66f9899493f4074
teagang <42443593+teagang@users.noreply.github.com>
2020-12-27 21:22:13 +0200

    day 16 testimonials exist, captcha needs work

--- index.js
+++ index.js
@@ -15,3 +15,25 @@ $('.navbar-collapse a').click(function(){
 
 
 
+/* this is a callback function to handle the token*/
+
+
+function onSubmit(token) {
+  document.getElementById("contactform").submit();
+}
+
+/*API response:*/
+{
+    "success": true|false,
+    "challenge_ts": timestamp,  // timestamp of the challenge load (ISO format yyyy-MM-dd'T'HH:mm:ssZZ)
+    "hostname": string,         // the hostname of the site where the reCAPTCHA was solved
+    "error-codes": [...]        // optional
+  }
+
+  /*for android*/
+  {
+    "success": true|false,
+    "challenge_ts": timestamp,  // timestamp of the challenge load (ISO format yyyy-MM-dd'T'HH:mm:ssZZ)
+    "apk_package_name": string, // the package name of the app where the reCAPTCHA was solved
+    "error-codes": [...]        // optional
+  }
