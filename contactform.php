1bc46df08c9fbd4b07e060860b42ee5cc199493b
teagang <42443593+teagang@users.noreply.github.com>
2020-12-30 20:43:53 +0200

    day 17 php form

---
+++ contactform.php
@@ -0,0 +1,10 @@
+<html>
+    <body>
+
+    <?php echo $_POST["name"]; ?><br>
+    <?php echo $_POST["email"]; ?><br>
+    <?php echo $_POST["phone"]; ?><br>
+    <?php echo $_POST["notes"]; ?><br>
+
+</body>
+</html>
