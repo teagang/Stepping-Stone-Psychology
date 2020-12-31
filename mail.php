5c8ecadf74db064a59b364941c93f32b3be1d7bf
teagang <42443593+teagang@users.noreply.github.com>
2020-12-31 14:19:55 +0200

    edited php to have head etc

--- mail.php
+++ mail.php
@@ -1,3 +1,8 @@
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+<body>
 <?php $name = $_POST['name'];
 $email = $_POST['email'];
 $message = $_POST['message'];
@@ -8,3 +13,5 @@ $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
 echo "Thank You!";
 ?>
+</body>
+</html>
