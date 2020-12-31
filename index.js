a1c87c727b6cadc297fc425cc3afbf2c2a05f33b
teagang <42443593+teagang@users.noreply.github.com>
2020-12-31 11:36:10 +0200

    changed recaptcha

--- index.js
+++ index.js
@@ -15,19 +15,3 @@ $('.navbar-collapse a').click(function(){
 
 
 
-/* this is a callback function to handle the token*/
-
-
-function onSubmit(token) {
-  document.getElementById("contactform").submit();
-}
-
-/*API response:*/
-{
-    "success": true|false,
-    "challenge_ts": timestamp,  // timestamp of the challenge load (ISO format yyyy-MM-dd'T'HH:mm:ssZZ)
-    "hostname": steppingstonepsychology.com,         // the hostname of the site where the reCAPTCHA was solved
-    "error-codes": [...]        // optional
-  }
-
-  
--- mail.php
+++ mail.php
@@ -1,3 +1,26 @@
+<?php // Check if form was submitted:
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['recaptcha_response'])) {
+
+    // Build POST request:
+    $recaptcha_url = 'https://www.google.com/recaptcha/api/siteverify';
+    $recaptcha_secret = '6Le_RxUaAAAAAHHkm-3DNAWfzXKhzATIGm6H0rXm';
+    $recaptcha_response = $_POST['recaptcha_response'];
+
+    // Make and decode POST request:
+    $recaptcha = file_get_contents($recaptcha_url . '?secret=' . $recaptcha_secret . '&response=' . $recaptcha_response);
+    $recaptcha = json_decode($recaptcha);
+
+    // Take action based on the score returned:
+    if ($recaptcha->score >= 0.5) {
+        // Verified - send email
+    } else {
+        // Not verified - show form error
+    }
+
+} ?>
+
+
+
 <?php
 // define variables and set to empty values
 $nameErr = $emailErr = $phoneErr = $notesErr = "";
